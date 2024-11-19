e9a328a7f2e2123346c6a130c75a0941891664ca
Kevin Lenin Ordoñez Cabrera <138833988+KevinTitanZ@users.noreply.github.com>
2024-11-19 00:06:59 -0500

    Borrar

--- procesar_login.php
+++ procesar_login.php
@@ -1,27 +1,23 @@
 <?php
 session_start();
-include 'includes/db.php'; // Ruta a tu archivo de conexión a la base de datos
+include 'includes/db.php'; 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $contrasena = $_POST['contrasena'];
 
     try {
-        // Buscar al usuario por su email
         $sql = "SELECT * FROM usuarios WHERE email = :email";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(['email' => $email]);
         $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($usuario) {
-            // Validar la contraseña
             if (password_verify($contrasena, $usuario['contrasena'])) {
-                // Credenciales válidas
                 $_SESSION['id_usuario'] = $usuario['id'];
                 $_SESSION['rol'] = $usuario['rol'];
                 $_SESSION['nombre'] = $usuario['nombre'];
 
-                // Redirigir según el rol
                 if ($usuario['rol'] === 'administrador') {
                     header('Location: admin/dashboard.php');
                 } else {
